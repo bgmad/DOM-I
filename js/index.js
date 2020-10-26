04fac288d9338848351bf99d5dc9d603b2bd4d67
Ian Luchini <iancarloluchini@gmail.com>
2020-10-26 18:52:51 -0400

    cleaned comments and formated with Prettier

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 let lightMode = true;
 const siteContent = {
-  "nav": {
+  nav: {
     "nav-item-0": "Home",
     "nav-item-1": "Services",
     "nav-item-2": "Product",
@@ -9,153 +9,164 @@ const siteContent = {
     "nav-item-5": "About",
     "nav-item-6": "Contact",
     "nav-item-7": "Ideas",
-    "img-src": "img/logo.png"
+    "img-src": "img/logo.png",
   },
-  "cta": {
-    "h1": "DOM Is Awesome",
-    "button": "Get Started",
-    "img-src": "img/header-img.png"
+  cta: {
+    h1: "DOM Is Awesome",
+    button: "Get Started",
+    "img-src": "img/header-img.png",
   },
   "main-content": {
-    "features-h4":"Features",
-    "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
-    "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "features-h4": "Features",
+    "features-content":
+      "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "about-h4": "About",
+    "about-content":
+      "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "middle-img-src": "img/mid-page-accent.jpg",
-    "services-h4":"Services",
-    "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4":"Product",
-    "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4":"Vision",
-    "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "services-h4": "Services",
+    "services-content":
+      "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "product-h4": "Product",
+    "product-content":
+      "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "vision-h4": "Vision",
+    "vision-content":
+      "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
   },
-  "contact": {
-    "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street \nSomewhere, USA",
-    "phone" : "1 (888) 888-8888",
-    "email" : "sales@greatidea.io",
+  contact: {
+    "contact-h4": "Contact",
+    address: "123 Way 456 Street \nSomewhere, USA",
+    phone: "1 (888) 888-8888",
+    email: "sales@greatidea.io",
   },
-  "footer": {
-    "copyright" : "Copyright Great Idea! 2018"
+  footer: {
+    copyright: "Copyright Great Idea! 2018",
   },
 };
 
-
-
-print = toPrint => console.log(toPrint);
-
-
-
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"]);
+logo.setAttribute("src", siteContent["nav"]["img-src"]);
 
 //Container with every element on the page in it.
 
-//Header and all of the elements inside of the header. 
+//Header and all of the elements inside of the header.
 
-      const nav = document.querySelector('nav'); //as a nodelist not a real array
-      const newAnchor = document.createElement('a');
-      newAnchor.setAttribute('href', '#');
-      nav.prepend(newAnchor.cloneNode(true));
-      nav.appendChild(newAnchor.cloneNode(true));
-      const navLinks = document.querySelectorAll('nav a'); //as a nodelist not a real array
-      navLinks.forEach(link => link.style.color = 'green');
-      navLinks.forEach((link, i) => link.textContent = siteContent.nav['nav-item-' + i]);
+const nav = document.querySelector("nav"); //as a nodelist not a real array
+const newAnchor = document.createElement("a");
+newAnchor.setAttribute("href", "#");
+nav.prepend(newAnchor.cloneNode(true));
+nav.appendChild(newAnchor.cloneNode(true));
+const navLinks = document.querySelectorAll("nav a"); //as a nodelist not a real array
+navLinks.forEach((link) => (link.style.color = "green"));
+navLinks.forEach(
+  (link, i) => (link.textContent = siteContent.nav["nav-item-" + i])
+);
 
-  //CTA section and all of the elements in it.
-      const ctaHeader = document.querySelector('.cta-text h1');
-      ctaHeader.innerText = siteContent.cta.h1.split(' ').join('\n');
-      const ctaButton = document.querySelector('.cta-text button');
-      ctaButton.textContent = siteContent.cta.button;
-    const ctaImg = document.querySelector('#cta-img');
-    ctaImg.setAttribute('src', siteContent.cta["img-src"])
-  
-  //Main content section and all of the elements in it.
-  const mainContent = document.querySelector('.main-content');
-        const firstTopHeader = document.querySelector('.top-content .text-content:nth-of-type(1) h4');
-        firstTopHeader.textContent = siteContent['main-content']['features-h4'];
-        const firstTopParagraph = document.querySelector('.top-content .text-content:nth-of-type(1) p');
-        firstTopParagraph.textContent = siteContent['main-content']['features-content'];
+//CTA section and all of the elements in it.
+const ctaHeader = document.querySelector(".cta-text h1");
+ctaHeader.innerText = siteContent.cta.h1.split(" ").join("\n");
+const ctaButton = document.querySelector(".cta-text button");
+ctaButton.textContent = siteContent.cta.button;
+const ctaImg = document.querySelector("#cta-img");
+ctaImg.setAttribute("src", siteContent.cta["img-src"]);
 
-        const secondTopHeader = document.querySelector('.top-content .text-content:nth-of-type(2) h4');
-        secondTopHeader.textContent = siteContent['main-content']['about-h4'];
-        const secondTopParagraph = document.querySelector('.top-content .text-content:nth-of-type(2) p');
-        secondTopParagraph.textContent = siteContent['main-content']['about-content'];
+//Main content section and all of the elements in it.
+const mainContent = document.querySelector(".main-content");
+const firstTopHeader = document.querySelector(
+  ".top-content .text-content:nth-of-type(1) h4"
+);
+firstTopHeader.textContent = siteContent["main-content"]["features-h4"];
+const firstTopParagraph = document.querySelector(
+  ".top-content .text-content:nth-of-type(1) p"
+);
+firstTopParagraph.textContent = siteContent["main-content"]["features-content"];
 
-    const middleImg = document.querySelector('#middle-img');
-    middleImg.setAttribute('src', siteContent['main-content']['middle-img-src']);
+const secondTopHeader = document.querySelector(
+  ".top-content .text-content:nth-of-type(2) h4"
+);
+secondTopHeader.textContent = siteContent["main-content"]["about-h4"];
+const secondTopParagraph = document.querySelector(
+  ".top-content .text-content:nth-of-type(2) p"
+);
+secondTopParagraph.textContent = siteContent["main-content"]["about-content"];
 
-        const firstBottomHeader = document.querySelector('.bottom-content .text-content:nth-of-type(1) h4');
-        firstBottomHeader.textContent = siteContent['main-content']['services-h4'];
-        const firstBottomParagraph = document.querySelector('.bottom-content .text-content:nth-of-type(1) p');
-        firstBottomParagraph.textContent = siteContent['main-content']['services-content'];
+const middleImg = document.querySelector("#middle-img");
+middleImg.setAttribute("src", siteContent["main-content"]["middle-img-src"]);
 
-        const secondBottomHeader = document.querySelector('.bottom-content .text-content:nth-of-type(2) h4');
-        secondBottomHeader.textContent = siteContent['main-content']['product-h4'];
-        const secondBottomParagraph = document.querySelector('.bottom-content .text-content:nth-of-type(2) p');
-        secondBottomParagraph.textContent = siteContent['main-content']['product-content'];
+const firstBottomHeader = document.querySelector(
+  ".bottom-content .text-content:nth-of-type(1) h4"
+);
+firstBottomHeader.textContent = siteContent["main-content"]["services-h4"];
+const firstBottomParagraph = document.querySelector(
+  ".bottom-content .text-content:nth-of-type(1) p"
+);
+firstBottomParagraph.textContent =
+  siteContent["main-content"]["services-content"];
 
-        const thirdBottomHeader = document.querySelector('.bottom-content .text-content:nth-of-type(3) h4');
-        thirdBottomHeader.textContent = siteContent['main-content']['vision-h4'];
-        const thirdBottomParagraph = document.querySelector('.bottom-content .text-content:nth-of-type(3) p');
-        thirdBottomParagraph.textContent = siteContent['main-content']['vision-content'];
+const secondBottomHeader = document.querySelector(
+  ".bottom-content .text-content:nth-of-type(2) h4"
+);
+secondBottomHeader.textContent = siteContent["main-content"]["product-h4"];
+const secondBottomParagraph = document.querySelector(
+  ".bottom-content .text-content:nth-of-type(2) p"
+);
+secondBottomParagraph.textContent =
+  siteContent["main-content"]["product-content"];
 
-  
-  //contact section
-  const contactHeader = document.querySelector('.contact h4');
-  contactHeader.textContent = siteContent.contact['contact-h4'];
-  const firstContactParagraph = document.querySelector('.contact p:nth-of-type(1)');
-  firstContactParagraph.innerText = siteContent.contact.address;
-  const secondContactParagraph = document.querySelector('.contact p:nth-of-type(2)');
-  secondContactParagraph.textContent = siteContent.contact.phone;
-  const thirdContactParagraph = document.querySelector('.contact p:nth-of-type(3)');
-  thirdContactParagraph.textContent = siteContent.contact.email;
+const thirdBottomHeader = document.querySelector(
+  ".bottom-content .text-content:nth-of-type(3) h4"
+);
+thirdBottomHeader.textContent = siteContent["main-content"]["vision-h4"];
+const thirdBottomParagraph = document.querySelector(
+  ".bottom-content .text-content:nth-of-type(3) p"
+);
+thirdBottomParagraph.textContent =
+  siteContent["main-content"]["vision-content"];
 
-  //footer 
-  const button = document.createElement('button');
-  button.textContent = 'Night Mode';
+//contact section
+const contactHeader = document.querySelector(".contact h4");
+contactHeader.textContent = siteContent.contact["contact-h4"];
+const firstContactParagraph = document.querySelector(
+  ".contact p:nth-of-type(1)"
+);
+firstContactParagraph.innerText = siteContent.contact.address;
+const secondContactParagraph = document.querySelector(
+  ".contact p:nth-of-type(2)"
+);
+secondContactParagraph.textContent = siteContent.contact.phone;
+const thirdContactParagraph = document.querySelector(
+  ".contact p:nth-of-type(3)"
+);
+thirdContactParagraph.textContent = siteContent.contact.email;
 
-  button.style.border = '1px double black';
-  button.style.fontSize = '16px';
-  button.style.background = 'white';
-  button.style.width = '170px';
-  button.style.height = '32px';
-  button.style.marginTop = '10px';
-  button.style.cursor = 'pointer';
-  
-  const footer = document.querySelector('footer');
-  footer.appendChild(button);
+//footer
+const button = document.createElement("button");
+button.textContent = "Night Mode";
 
+button.style.border = "1px double black";
+button.style.fontSize = "16px";
+button.style.background = "white";
+button.style.width = "170px";
+button.style.height = "32px";
+button.style.marginTop = "10px";
+button.style.cursor = "pointer";
 
-  const footerParagraph = document.querySelector('footer p');
-  footerParagraph.textContent = siteContent.footer.copyright;
+const footer = document.querySelector("footer");
+footer.appendChild(button);
 
-  button.addEventListener('click', () => {
-    if (lightMode) {
-      lightMode = false;
-      document.querySelector('body').style.background = 'black';
-      document.querySelector('body').style.color = 'white';
-    } else if (!lightMode){
-      lightMode = true;
-      document.querySelector('body').style.background = 'white';
-      document.querySelector('body').style.color = 'black';
-    }
-  })
-  
+const footerParagraph = document.querySelector("footer p");
+footerParagraph.textContent = siteContent.footer.copyright;
 
-
-//   .cta .cta-text button {
-//     border: 1px double black;
-//     font-size: 16px;
-//     background: white;
-//     width: 170px;
-//     height: 32px;
-//     margin-top: 10px;
-//     cursor: pointer;
-// }
-
-// .cta .cta-text button:hover {
-//     background: black;
-//     color: white;
-// }
+button.addEventListener("click", () => {
+  if (lightMode) {
+    lightMode = false;
+    document.querySelector("body").style.background = "black";
+    document.querySelector("body").style.color = "white";
+  } else if (!lightMode) {
+    lightMode = true;
+    document.querySelector("body").style.background = "white";
+    document.querySelector("body").style.color = "black";
+  }
+});
